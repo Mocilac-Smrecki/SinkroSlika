871582fef1c52fd54f5365c99065f6504a0d00bc
Tomislav Smrecki <smrecki.tomislav@gmail.com>
2013-04-05 12:41:02 +0200

    Loptica
    Ubacen primjer s skakanjem loptice
    Prikazuje se u LinearView-u

--- src/hr/fer/zari/sinkroslike/FullscreenActivity.java
+++ src/hr/fer/zari/sinkroslike/FullscreenActivity.java
@@ -4,11 +4,16 @@ import hr.fer.zari.sinkroslike.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -48,12 +53,13 @@ public class FullscreenActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.activity_fullscreen);
 
 		final View controlsView = findViewById(R.id.fullscreen_content_controls);
-		final View contentView = findViewById(R.id.fullscreen_content);
-
+		//final View contentView = findViewById(R.id.fullscreen_content);
+		LinearLayout contentView = (LinearLayout) findViewById(R.id.fullscreen_content);
+		BallBounce b = new BallBounce (this);
+		contentView.addView(b);
 		// Set up an instance of SystemUiHider to control the system UI for
 		// this activity.
 		mSystemUiHider = SystemUiHider.getInstance(this, contentView,
@@ -98,7 +104,8 @@ public class FullscreenActivity extends Activity {
 						}
 					}
 				});
-
+		
+		
 		// Set up the user interaction to manually show or hide the system UI.
 		contentView.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -110,6 +117,8 @@ public class FullscreenActivity extends Activity {
 				}
 			}
 		});
+		
+	
 
 		// Upon interacting with UI controls, delay any scheduled hide()
 		// operations to prevent the jarring behavior of controls going away
@@ -117,7 +126,7 @@ public class FullscreenActivity extends Activity {
 		findViewById(R.id.dummy_button).setOnTouchListener(
 				mDelayHideTouchListener);
 	}
-
+	
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
@@ -159,4 +168,70 @@ public class FullscreenActivity extends Activity {
 		mHideHandler.removeCallbacks(mHideRunnable);
 		mHideHandler.postDelayed(mHideRunnable, delayMillis);
 	}
+	
+	class BallBounce extends View {
+		int screenW;
+		int screenH;
+		int X;
+		int Y;
+		int initialY;
+		int ballW;
+		int ballH;
+		int angle;
+		double dY;
+		float acc;
+		Bitmap ball, bgr;
+		
+		public BallBounce(Context context) {
+	        super(context);
+	        ball = BitmapFactory.decodeResource(getResources(),R.drawable.football); //load a ball image
+	        bgr = BitmapFactory.decodeResource(getResources(),R.drawable.sky_bgr); //load a background
+	        ballW = ball.getWidth();
+	        ballH = ball.getHeight();
+	        acc = 0.2f; //acceleration
+	        dY = 0; //vertical speed
+	        initialY = 100; //Initial vertical position.
+	        angle = 0; //Start value for rotation angle.
+	    }
+
+	    @Override
+	    public void onSizeChanged (int w, int h, int oldw, int oldh) {
+	        super.onSizeChanged(w, h, oldw, oldh);
+	        screenW = w;
+	        screenH = h;
+	        bgr = Bitmap.createScaledBitmap(bgr, w, h, true); //Resize background to fit the screen.
+	        X = (int) (screenW /2) - (ballW / 2) ; //Centre ball into the centre of the screen.
+	        Y = initialY;
+	    }
+
+	    @Override
+	    public void onDraw(Canvas canvas) {
+	        super.onDraw(canvas);
+
+	        //Draw background.
+	        canvas.drawBitmap(bgr, 0, 0, null);
+
+	        //Compute roughly ball speed and location.
+	        Y+= (int) dY; //Increase or decrease vertical position.
+	        if (Y > (screenH - ballH)) {
+	            dY=(-0.9)*dY; //Reverse speed when bottom hit.
+	        }
+	        dY+= acc; //Increase or decrease speed.
+
+	        //Increase rotating angle.
+	        if (angle++ >360)
+	            angle =0;
+
+	        //Draw ball
+	        canvas.save(); //Save the position of the canvas.
+	        canvas.rotate(angle, X + (ballW / 2), Y + (ballH / 2)); //Rotate the canvas.
+	        canvas.drawBitmap(ball, X, Y, null); //Draw the ball on the rotated canvas.
+	        canvas.restore(); //Rotate the canvas back so that it looks like ball has rotated.
+
+	        //Call the next frame.
+	        invalidate();
+	    }
+		
+	}
+	
 }
