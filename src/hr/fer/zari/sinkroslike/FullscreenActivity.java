698fbe31f915a2aa4bf10bcf0ae18ec29b76f553
Matija Močilac <mocilac.matija@gmail.com>
2013-06-04 22:22:13 +0200

    Dodani komentrai i izbrisani suvišni komentari

--- src/hr/fer/zari/sinkroslike/FullscreenActivity.java
+++ src/hr/fer/zari/sinkroslike/FullscreenActivity.java
@@ -1,24 +1,19 @@
 package hr.fer.zari.sinkroslike;
 
+import hr.fer.zari.sinkroslike.util.SystemUiHider;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
-import hr.fer.zari.sinkroslike.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -32,7 +27,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.os.AsyncTask;
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
@@ -63,19 +57,19 @@ public class FullscreenActivity extends Activity {
 	 */
 	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
 	
-	private TextView text;
-	private Button send;
-	private Button settings;
+	private TextView text;					    //prikazuje poruke sa servera
+	private Button send;						//gumb za spajanje na server
+	private Button settings;					//gumb za postavljanje IP adrese servera
 	private int numOfDevices = 0;
 	private int DeviceNumber = 0;
 
-	private boolean returnFromRight = false;
-	private boolean returnFromLeft = false;
-	private boolean sentNext = false;
-	private boolean sentPrevious = false;
+	private boolean returnFromRight = false;	//oznaèava da je došlo do promjene smjera s desna na lijevo
+	private boolean returnFromLeft = false;		//oznaèava da je došlo do promjene smjera s lijeva na desno
+	private boolean sentNext = false;			//oznaèava da je poslana poruka iduæem mobitelu da pokrene patkicu (kada je smjer kretanja s lijeva na desno)
+	private boolean sentPrevious = false;		//oznaèava da je poslana poruka prethodnom mobitelu da pokrene patkicu (kada je smjer kretanja s desna na lijevo)
 	private String smjer = "right";
-	private boolean boostDuck = false;
-	private boolean initX = false;
+	private boolean zoomDuck = false;			//oznaèava da patkicu treba uveæati
+	private boolean initX = false;				//oznaèava da je poèetna koordinata patkice inicijalizirana
 	
 	private ClientZaAndroid c;
 	private String ipAdress = "161.53.67.220";
@@ -85,7 +79,7 @@ public class FullscreenActivity extends Activity {
 	 * The instance of the {@link SystemUiHider} for this activity.
 	 */
 	private SystemUiHider mSystemUiHider;
-	private Handler handler = new Handler();
+	private Handler handler = new Handler();		//razred Handler služi za slanje poruka izmeðu dretvi
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -93,13 +87,12 @@ public class FullscreenActivity extends Activity {
 		setContentView(R.layout.activity_fullscreen);
 		
 		final View controlsView = findViewById(R.id.fullscreen_content_controls);
-		//final View contentView = findViewById(R.id.fullscreen_content);
 		LinearLayout contentView = (LinearLayout) findViewById(R.id.fullscreen_content);
 		text = (TextView) findViewById(R.id.textView1);
-		send = (Button) findViewById(R.id.send);
-		settings = (Button) findViewById (R.id.settings);
-		DuckFloating b = new DuckFloating (this);
-		contentView.addView(b);
+		send = (Button) findViewById(R.id.send); 
+		settings = (Button) findViewById (R.id.settings);	
+		DuckFloating duck = new DuckFloating (this);
+		contentView.addView(duck);
 		
 		// Set up an instance of SystemUiHider to control the system UI for
 		// this activity.
@@ -167,7 +160,7 @@ public class FullscreenActivity extends Activity {
 		findViewById(R.id.dummy_button).setOnTouchListener(
 				mDelayHideTouchListener);
 		
-		
+		//klikom na gumb send stvori novu dretvu klijenta koja æe komunicirati sa serverom
 		send.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -179,6 +172,7 @@ public class FullscreenActivity extends Activity {
 			}
 		});
 		
+		//gumb za unos IP adrese servera
 		settings.setOnClickListener(new View.OnClickListener() {
 			
 			
@@ -273,21 +267,18 @@ public class FullscreenActivity extends Activity {
 		double dX;
 		float acc;
 		Bitmap duck, bgr;
-		boolean maxAngle = false;
-		boolean scaleDuck = false;
+		boolean maxAngle = false;			//oznaèava maksimalni kut do kojega se patkica može rotirati
+		boolean scaleDuck = false;			//oznaèava da patkica treba promjeniti smjer kretanja (tj. da treba primjeniti matricu skaliranja)
 		
 		public DuckFloating(Context context) {
 	        super(context);
-	        duck = BitmapFactory.decodeResource(getResources(),R.drawable.duck); //load a duck image
-	        bgr = BitmapFactory.decodeResource(getResources(),R.drawable.river); //load a background
+	        duck = BitmapFactory.decodeResource(getResources(),R.drawable.duck); //uèitaj sliku patkice
+	        bgr = BitmapFactory.decodeResource(getResources(),R.drawable.river); //uèitaj pozadinsku sliku
 	        duckW = duck.getWidth();
 	        duckH = duck.getHeight();
-	        acc = 0.2f; //acceleration
-	        dY = 0; //vertical speed
-	        initialY = screenH; //Initial vertical position.
 	        initialX = - (duckW + 2);
-	        angle = 0; //Start value for rotation angle.
-	        dX = 1;
+	        angle = 0; //poèetni kut rotacije.
+	        dX = 1;	   //pomak po osi X
 	    }
 
 	    @Override
@@ -295,31 +286,23 @@ public class FullscreenActivity extends Activity {
 	        super.onSizeChanged(w, h, oldw, oldh);
 	        screenW = w;
 	        screenH = h;
-	        bgr = Bitmap.createScaledBitmap(bgr, w, h, true); //Resize background to fit the screen.
-	        X = initialX; //Centre duck into the centre of the screen.
-	        Y = screenH - duckH - duckH/2;
+	        bgr = Bitmap.createScaledBitmap(bgr, w, h, true); //prikaži pozadinu preko cijelog zaslona
+	        X = initialX;
+	        Y = screenH - duckH - duckH/2;		//Y koordinata patkice; ne mijenja se tijekom izvoðenja programa
 	    }
 
 	    @Override
 	    public void onDraw(Canvas canvas) {
 	        super.onDraw(canvas);
 
-	        //Draw background.
+	        //Nacrtaj pozadinu.
 	        canvas.drawBitmap(bgr, 0, 0, null);
-	        
-	        /*
-	        //Compute roughly duck speed and location.
-	        Y+= (int) dY; //Increase or decrease vertical position.
-	        if (Y > (screenH - duckH)) {
-	            dY=(-1)*dY; //Reverse speed when bottom hit.
-	        }
-	        dY+= acc; //Increase or decrease speed.
-			*/
 	        
 	        if (DeviceNumber == 0)
 	        {
 	        	X = (- duckW) - 10;
 	        }
+	        
 	        else if (DeviceNumber == 1 && initX == false)
 	        {
 	        	X = 0;
@@ -337,17 +320,19 @@ public class FullscreenActivity extends Activity {
 	        
 	        X += (int) dX;
    
-	        // kretanje u desno, if not krajnji desni, else if krajnji desni
+	        // ako je patkica doplivala do desnog ruba zaslona, a mobitel nije krajnje desni
 	        if (X > (screenW - duckW) && DeviceNumber < numOfDevices && sentNext == false && smjer.equals("right"))
 	        {
 	        	Integer nextDev = DeviceNumber + 1;
         		String nextDevice = nextDev.toString();
-        		c.sendMessage(nextDevice);
+        		c.sendMessage(nextDevice);				//pošalji poruku s rednim brojem iduæeg mobitela
         		sentNext = true;
 	        }
+	        //ako je patkica "otplivala" izvan desnog ruba zaslona, a mobitel nije krajnji desni
 	        else if (X > screenW && DeviceNumber < numOfDevices && smjer.equals("right"))
 	        {
-	        	X -= (int) dX;
+	        	X -= (int) dX; /*zadrži patkicu na istoj poziciji tako da u svakoj iteraciji umanjiš X koordinatu za pomak dX. (buduæi da se u svakoj iteraciji X
+	        	 				koordinata najprije uveæava za dX, a na ovom mjestu se smanjuje za isti iznos, to æe za posljedicu imati da se patkica neæe kretati*/
 	        	if (returnFromRight == true)
 	        	{
 	        		dX = (-1)*dX;
@@ -358,6 +343,7 @@ public class FullscreenActivity extends Activity {
 	        		
 	        	}
 	        }
+	        //ako je mobitel krajnje desni i patkica je doplivala do desnog ruba zaslona, okreni ju na drugu stranu
 	        else if (DeviceNumber == numOfDevices && X > (screenW - duckW))
 	        {
 	        	dX = (-1)*dX;
@@ -368,9 +354,11 @@ public class FullscreenActivity extends Activity {
 	        
 	        
 	        // kretanje u lijevo
+	        
+	        //ako je patkica otplivala izvan lijevog ruba zaslona, a mobitel nije krajnje lijevi
 	        if (X < -duckW && DeviceNumber > 1 && smjer.equals("left"))
 	        {
-	        	X -= (int) dX;
+	        	X -= (int) dX;	//zadrži patkicu na istom mjestu po istoj logici kao i nekoliko redaka iznad
 	        	if (returnFromLeft == true)
 	        	{
 	        		dX = (-1)*dX;
@@ -381,13 +369,16 @@ public class FullscreenActivity extends Activity {
 	        		
 	        	}
 	        }
+	        //ako je patkica doplivala do lijevog ruba zaslona, a mobitel nije krajnje lijevi
 	        else if (X < 0 && DeviceNumber > 1 && sentPrevious == false && smjer.equals("left"))
 	        {
 	        	Integer previousDev = DeviceNumber - 1;
         		String prevDevice = previousDev.toString();
-        		c.sendMessage(prevDevice);
+        		c.sendMessage(prevDevice);			//pošalji poruku s rednim brojem iduæeg mobitela na kojemu se patkica treba kretati
         		sentPrevious = true;
 	        }
+	        
+	        //ako je patkica doplivala do lijevog ruba zaslona i mobitel je krajnje lijevi, okreni patkicu
 	        else if (DeviceNumber == 1 && X < 0)
 	        {
 	        	dX = (-1)*dX;
@@ -397,35 +388,34 @@ public class FullscreenActivity extends Activity {
 	        }
 	        
      
-	        //Increase rotating angle.
+	        //Mijenjaj kut rotacije
 	        if (angle > 15) maxAngle = true;
 	        if (maxAngle == false) angle += 0.5;
 	        else angle -= 0.5;
 	        if (angle < -10) maxAngle = false;
 	        
 	        
-	        canvas.save(); //Save the position of the canvas
-	        //Draw duck
+	        canvas.save(); //Spremi trenutnu matricu na stog
 	        if (scaleDuck == true)
 	        	{
 	        	canvas.scale(-1.0f, 1.0f, X + (duckW /2), Y + (duckH /2));	 
 	        	}
-	        if (boostDuck == true)
+	        if (zoomDuck == true)
 	        {
 	        	canvas.scale(1.5f, 1.5f, X + (duckW /2), Y + (duckH /2));
-	        	boostDuck = false;
+	        	zoomDuck = false;
 	        }
-	        canvas.rotate(angle, X + (duckW / 2), Y + (duckH / 2)); //Rotate the canvas.
-	        canvas.drawBitmap(duck, X, Y, null); //Draw the duck on the rotated canvas.
-	        canvas.restore(); //Rotate the canvas back so that it looks like duck has rotated.
+	        canvas.rotate(angle, X + (duckW / 2), Y + (duckH / 2)); //Rotiraj patkicu
+	        canvas.drawBitmap(duck, X, Y, null); //Nacrtaj patkicu na koordinatama X, Y
+	        canvas.restore(); //Poništi sve transformacije nad trenutnom matricom
 
-	        //Call the next frame.
+	        //Nacrtaj iduæu iteraciju
 	        invalidate();
 	    }
 		
 	}
 	
-	// ******************* connect to internet ************************ 
+	// ******************* spajanje na server ************************ 
 
 	public class ClientZaAndroid extends Thread {
 
@@ -435,7 +425,7 @@ public class FullscreenActivity extends Activity {
 	    private PrintWriter outputMessage = null;
 	    private BufferedReader inputMessage = null;
 	    private String serverMessage;
-	    private Boolean ide = true;
+	    private Boolean ide = true;				//oznaèava da je dretva klijenta pokrenuta
 
 		public ClientZaAndroid(String threadName, String ip, String port){
 			super(threadName);
@@ -459,19 +449,18 @@ public class FullscreenActivity extends Activity {
 			
 			try {
 				socket = new Socket(serverIP, serverPort);
-				outputMessage = new PrintWriter(socket.getOutputStream(), true);
-				inputMessage = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				outputMessage = new PrintWriter(socket.getOutputStream(), true);					//za slanje poruka na server
+				inputMessage = new BufferedReader(new InputStreamReader(socket.getInputStream()));	//za primanje poruka sa servera
 				
 				serverMessage = inputMessage.readLine();
 				
 				handler.post(new Runnable() {
                     @Override
                     public void run() {
-                    	
                     	String DeviceNum = serverMessage.split(",")[0];
                     	String numDevice = serverMessage.split(",")[1];
                     	DeviceNumber = Integer.parseInt(DeviceNum);
-                    	numOfDevices = Integer.parseInt(numDevice); 
+                    	numOfDevices = Integer.parseInt(numDevice);
                     	text.setText(serverMessage);
                     	
                     }
@@ -489,7 +478,7 @@ public class FullscreenActivity extends Activity {
                             	
                             	if (serverMessage.equals("left")) returnFromRight = true;
                             	else if (serverMessage.equals("right")) returnFromLeft = true;
-                            	else if (serverMessage.equals("signal")) boostDuck = true;
+                            	else if (serverMessage.equals("signal")) zoomDuck = true;
                                 text.setText(serverMessage);
                             }
                         });
